746debe2ba4ac93a049ad963e47e8dbb43338ff8
Jordan Reed <jordanr23@gmail.com>
2018-01-23 20:48:02 -0700

    apollo server configured

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,13 +1,31 @@
 import React, {Component} from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 import './Main.css';
 // import { Switch, Route } from 'react-router-dom'
 // import './App.css';
 // import Login from './Login'
 // import {Link} from 'react-router-dom'
 
+const allUsersQuery = gql`{
+  allUsers{
+    id
+    username
+    pic
+  }
+}
+`;
+
 class Main extends Component {
   render() {
+    let {data} = this.props;
+    if(data.loading) {
+      return <div>Loading...</div>
+    }
     return (<div className="Main">
+      {data.allUsers.map(x => (
+        <h1 key={x.id}>{x.username}</h1>
+      ))}
       {/* <div className="Main-left">
         <Switch>
           <Route exact="exact" path='/' component={Home}/>
@@ -23,4 +41,5 @@ class Main extends Component {
   }
 }
 
+Main = graphql(allUsersQuery)(Main);
 export default Main;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
+
+import {ApolloClient} from 'apollo-client';
+import {createHttpLink} from 'apollo-link-http';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {ApolloProvider} from 'react-apollo';
+
 import registerServiceWorker from './registerServiceWorker';
 
+const client = new ApolloClient({
+  link: createHttpLink({uri: 'http://localhost:8081/graphql'}),
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render((
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter>
-), document.getElementById('root'));
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>
+  ), document.getElementById('root'));
 registerServiceWorker();
